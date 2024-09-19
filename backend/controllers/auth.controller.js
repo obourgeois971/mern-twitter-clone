47cf8e3e3ec97962c2db3a50aac5c48e64fbc5c1
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-09-19 12:53:15 +0200

    auth routes and controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  res.json({ data: "You hit the signup endpoint" }); // TODO: implement signup
+};
+
+export const login = (req, res) => {
+  res.json({ data: "You hit the login endpoint" }); // TODO: implement login
+};
+
+export const logout = (req, res) => {
+  res.json({ data: "You hit the logout endpoint" }); // TODO: implement logout
+};
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("Server is up and running");
-});
+// console.log(process.env.MONGO_URI);
 
-app.listen(8000, () => {
-  console.log("server is running on port 8000");
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+  connectMongoDB();
 });
